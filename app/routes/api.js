ebbb033e6f45c3c59dbc962eae551ef3b2137138
Jean-Baptiste <contact@jbpasquier.eu>
2016-04-20 09:38:34 +0200

    Git init

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,7 +1,7 @@
 var Api = require('../models/api.js');
 module.exports 	= function(app) {
 
-	app.get('/api', Apis.findAll);
+	app.get('/api', Api.findAll);
 	app.post('/api', Api.create);
 	app.put('/api/:id', Api.update);
 	app.delete('/api/:id', Api.delete);
