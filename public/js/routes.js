373c1eccc2a330058a45bfd1af074bf55c9378d8
Jean-Baptiste <contact@jbpasquier.eu>
2016-05-09 09:11:52 +0200

    Search controller + route

--- public/js/routes.js
+++ public/js/routes.js
@@ -4,6 +4,10 @@ function routes($routeProvider) {
             templateUrl: 'views/main.html',
             controller: 'mainController'
         })
+        .when('/search', {
+            templateUrl: 'views/search.html',
+            controller: 'searchController'
+        })
         .when('/place/:id', {
             templateUrl: 'views/place-page.html',
             controller: 'placesController'
