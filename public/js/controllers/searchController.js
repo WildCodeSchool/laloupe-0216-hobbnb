8a261ecb7f4f466a5a751a04fdb94804261dcd0b
Paul Crayssac <paul-ka@laposte.net>
2016-05-24 16:03:26 +0200

    Add autocompletion spaces and bbq checkbox

--- public/js/controllers/searchController.js
+++ public/js/controllers/searchController.js
@@ -1,6 +1,8 @@
 angular.module('app').controller('searchController', function($scope, $http, placesFactory, placesService, NgMap) {
     $scope.hobbiesListing = ['Peu importe', "Randonnée", "VTT", "Cyclisme", "Equitation", "Pêche", "Plongée", "Golf", "Escalade", "Canoë Kayak", "Surf", "Stand up Paddle", "Kitesurf", "Windsurf", "Ski", "Alpinisme", "Parapente", "Spéléologie", "Cannoning"];
     $scope.formHobby = 'Choisissez un hobby...';
+    $scope.propertyTypeListing = ["Hebergement","Maison","Appartement","Chambre","Couchage","Place de camping","Cabane dans les arbres","Camping car","Tipy","Bateau","Yourte"]
+
     $scope.filters = {
         minPrice: 0,
         maxPrice: 1000,
@@ -11,14 +13,24 @@ angular.module('app').controller('searchController', function($scope, $http, pla
     };
     $scope.definitiveFilter = {};
     $scope.$watchCollection('filters', function(newCol, oldCol) {
-        if (!!newCol.hobby) {
-            if (newCol.hobby == 'Peu importe') {
-                $scope.filters.hobby = '';
-                if (!!$scope.definitiveFilter.primarySports) delete $scope.definitiveFilter.primarySports;
-            } else {
-                $scope.definitiveFilter.primarySports = newCol.hobby;
-            }
-        }
+      if (!!newCol.hobby) {
+          if (newCol.hobby == 'Peu importe') {
+              $scope.filters.hobby = '';
+              if (!!$scope.definitiveFilter.primarySports) delete $scope.definitiveFilter.primarySports;
+          } else {
+              $scope.definitiveFilter.primarySports = newCol.hobby;
+          }
+      }
+      if (!!newCol.propertyType) {
+          if (newCol.propertyType == 'Hebergement') {
+              $scope.filters.propertyType = '';
+              if (!!$scope.definitiveFilter.home.houseSpace.propertyType) delete $scope.definitiveFilter.home.houseSpace.propertyType;
+          } else {
+              if(!$scope.definitiveFilter.home) $scope.definitiveFilter.home = [];
+              if(!$scope.definitiveFilter.home.houseSpace) $scope.definitiveFilter.home.houseSpace = [];
+              $scope.definitiveFilter.home.houseSpace.propertyType = newCol.propertyType;
+          }
+      }
         if (!!newCol.beds) {
             if(!$scope.definitiveFilter.home) $scope.definitiveFilter.home = [];
             if(!$scope.definitiveFilter.home.houseSpace) $scope.definitiveFilter.home.houseSpace = [];
@@ -51,14 +63,34 @@ angular.module('app').controller('searchController', function($scope, $http, pla
           if(!$scope.definitiveFilter.home.houseAmenities) $scope.definitiveFilter.home.houseAmenities = [];
           if(!!$scope.definitiveFilter.home.houseAmenities.tv) delete $scope.definitiveFilter.home.houseAmenities.tv;
         }
+        if (!!newCol.hasEssentials) {
+          if(!$scope.definitiveFilter.home) $scope.definitiveFilter.home = [];
+          if(!$scope.definitiveFilter.home.houseAmenities) $scope.definitiveFilter.home.houseAmenities = [];
+            $scope.definitiveFilter.home.houseAmenities.essentials = newCol.hasEssentials;
+        } else {
+          if(!$scope.definitiveFilter.home) $scope.definitiveFilter.home = [];
+          if(!$scope.definitiveFilter.home.houseAmenities) $scope.definitiveFilter.home.houseAmenities = [];
+          if(!!$scope.definitiveFilter.home.houseAmenities.essentials) delete $scope.definitiveFilter.home.houseAmenities.essentials;
+        }
+        if (!!newCol.hasBbq) {
+          if(!$scope.definitiveFilter.home) $scope.definitiveFilter.home = [];
+          if(!$scope.definitiveFilter.home.houseAmenities) $scope.definitiveFilter.home.houseAmenities = [];
+            $scope.definitiveFilter.home.houseAmenities.bbq = newCol.hasBbq;
+        } else {
+          if(!$scope.definitiveFilter.home) $scope.definitiveFilter.home = [];
+          if(!$scope.definitiveFilter.home.houseAmenities) $scope.definitiveFilter.home.houseAmenities = [];
+          if(!!$scope.definitiveFilter.home.houseAmenities.bbq) delete $scope.definitiveFilter.home.houseAmenities.bbq;
+        }
         if (!!newCol.place) {
-            $scope.latitude = 48;
-            $scope.longitude = -3.362480;
+            $http.get('https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAamy-iuBFyQu-qNuihVdTQDCPyaqzrUac&address='+newCol.place).then(function(res) {
+            $scope.latitude = res.data.results[1].geometry.location.lat;
+            $scope.longitude = res.data.results[1].geometry.location.lon;
             $scope.latitudemin = $scope.latitude - 0.5;
             $scope.latitudemax = $scope.latitude + 0.5;
             $scope.kmbydegree = (111 * Math.cos($scope.latitude));
             $scope.longitudemin = $scope.longitude + 35 / $scope.kmbydegree;
             $scope.longitudemax = $scope.longitude - 35 / $scope.kmbydegree;
+          })
         } else {
             if (!!$scope.longitudemin) delete $scope.longitudemin;
             if (!!$scope.longitudemax) delete $scope.longitudemax;
@@ -144,12 +176,12 @@ angular.module('app').controller('searchController', function($scope, $http, pla
     $scope.toggleInfoWindow = function(event, id) {
         $scope.map.showInfoWindow('popup', this);
         $scope.indexOfTheTruc = id;
-        $scope.globalRating = $scope.howManyPositive($scope.positions[id].rating.valueForMoney.concat($scope.positions[id].rating.location, $scope.positions[id].rating.cleaness));
+        $scope.globalRating = $scope.howManyPositive($scope.positions[id].rating.valueForMoney.concat($scope.positions[id].rating.location, $scope.positions[id].rating.cleanness));
         $scope.globalLowerRating = 5 - $scope.globalRating;
-        $scope.reviewNb = ~~(($scope.positions[id].rating.cleaness.length + $scope.positions[id].rating.location.length + $scope.positions[id].rating.valueForMoney.length) / 3)
+        $scope.reviewNb = ~~(($scope.positions[id].rating.cleanness.length + $scope.positions[id].rating.location.length + $scope.positions[id].rating.valueForMoney.length) / 3)
     }
     $scope.calculStars = function(widget) {
-        $scope.globalRating = $scope.howManyPositive(widget.valueForMoney.concat(widget.location, widget.cleaness));
+        $scope.globalRating = $scope.howManyPositive(widget.valueForMoney.concat(widget.location, widget.cleanness));
         var resul = "";
         for (var i = 0; i < $scope.globalRating; i++) {
             resul += "star "
@@ -165,79 +197,15 @@ angular.module('app').controller('searchController', function($scope, $http, pla
         return resul;
     }
     $scope.nbReview = function(widget) {
-        return ~~((widget.cleaness.length + widget.location.length + widget.valueForMoney.length) / 3);
+        return ~~((widget.cleanness.length + widget.location.length + widget.valueForMoney.length) / 3);
     }
     $scope.hobbyIco = function(widget) {
         return "../assets/hobbies/" + widget.primarySports + ".png"
     }
 
-    //add tile in map
-    $scope.positions = [{
-        shortDescription: "Lorient",
-        latitude: 47.791859,
-        longitude: -3.362480,
-        picture: "../assets/places/place1.jpg",
-        rating: {
-            valueForMoney: [3],
-            location: [4],
-            cleaness: [5]
-        },
-        home: {
-            price: 57,
-            houseSpace: {
-                beds: 5
-            },
-            houseAmenities: {
-                kitchen: false,
-                wifi: true,
-                tv: false
-            }
-        },
-        primarySports: "Surf"
-    }, {
-        shortDescription: "Concarneau",
-        latitude: 47.883774,
-        longitude: -3.909815,
-        picture: "../assets/places/place2.jpg",
-        rating: {
-            valueForMoney: [5],
-            location: [5],
-            cleaness: [5]
-        },
-        home: {
-            price: 60,
-            houseSpace: {
-                beds: 2
-            },
-            houseAmenities: {
-                kitchen: true,
-                wifi: false,
-                tv: false
-            }
-        },
-        primarySports: "Kitesurf"
-    }, {
-        shortDescription: "Carnac",
-        latitude: 47.579523,
-        longitude: -3.078903,
-        picture: "../assets/places/place3.jpg",
-        rating: {
-            valueForMoney: [3],
-            location: [3],
-            cleaness: [3]
-        },
-        home: {
-            price: 530,
-            houseSpace: {
-                beds: 2
-            },
-            houseAmenities: {
-                kitchen: true,
-                wifi: true,
-                tv: true
-            }
-        },
-        primarySports: "Kitesurf"
-    }, ];
+    //Load positions
+    placesService.get().then(function(res){
+			$scope.positions = res.data;
+		});
 
 });
