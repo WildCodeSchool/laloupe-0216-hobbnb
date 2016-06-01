3b2684d6dae366e02338483b8b0fbbf56b99aad0
Jean-Baptiste <contact@jbpasquier.eu>
2016-06-01 09:34:21 +0200

    Clean file upload (picture)

--- public/js/controllers/fileUploadController.js
+++ public/js/controllers/fileUploadController.js
@@ -5,9 +5,11 @@ angular.module('app').controller('fileUploadController', function($scope, $http,
     if($routeParams.step == 0) {
         $scope.width = 1900;
         $scope.height = 400;
+        $scope.picType = 'de couverture (1900x400px)';
     } else {
         $scope.width = 600;
         $scope.height = 600;
+        $scope.picType = ' 600x600, jusqu\'à 6 photos';
     }
     $scope.title = $routeParams.where + "/" + $routeParams.id;
 });
