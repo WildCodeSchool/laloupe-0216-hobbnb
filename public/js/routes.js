dc082e07f356863cfe85dcff4e976804dbc7de1a
the-dsw <fmod@the-dsw.com>
2016-06-01 18:10:19 +0200

    send an email from our App

--- public/js/routes.js
+++ public/js/routes.js
@@ -215,6 +215,20 @@ function routes($routeProvider, $httpProvider) {
                 }]
             }
         })
+        .when('/sendEmail/', {
+            templateUrl: 'views/sendEmail.html',
+            controller: 'sendEmailController',
+            resolve: {
+                lazy: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'app',
+                        files: [
+                            'js/controllers/sendEmailController.js',
+                        ]
+                    });
+                }]
+            }
+        })
         .otherwise({
             redirectTo: '/'
         });
