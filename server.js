59595fcfa2ac6c55a285741b67937607e7b0f2ef
Jean-Baptiste <contact@jbpasquier.eu>
2016-06-02 10:59:57 +0200

    Duplicate bodyParser

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({
     'extended': 'true'
 })); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({
     type: 'application/vnd.api+json'
 })); // parse application/vnd.api+json as json
