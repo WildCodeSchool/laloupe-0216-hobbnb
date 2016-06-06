f8ff0f6f5144196f8b1775fb1672e54439c7b13e
Paul Crayssac <paul-ka@laposte.net>
2016-06-06 14:05:42 +0200

    link spot page with users base

--- public/js/controllers/spotsController.js
+++ public/js/controllers/spotsController.js
@@ -1,11 +1,30 @@
-angular.module('app').controller('spotsController', function($scope, $http, $location, $routeParams, spotsService) {
+angular.module('app').controller('spotsController', function($scope, $http, $location, $routeParams, spotsFactory, spotsService, usersService, searchFactory) {
 
   $scope.currentHost = $routeParams.id;
   $scope.howManyPositive = function(t) {
-      return !!t ? (~~(t.reduce(function(a,b){return a+b;}) / t.length) || 0) : 0;
+      return !!t ? (~~(t.reduce(function(a,b){
+          return a+b;
+      }) / t.length) || 0) : 0;
   };
   spotsService.getOne($scope.currentHost).then(function(e) {
+
       if(!e.data.name) $location.path('/spot');
+
+      usersService.getOne(e.data.owner).then(function(res) {
+          $scope.owner = res.data;
+          if ($scope.owner.rating.length <= 0) {
+              $scope.owner.rating = [3];
+          }
+          $scope.owner.globalRating = $scope.howManyPositive($scope.owner.rating);
+          $scope.owner.globalLowerRating = 5 - $scope.owner.globalRating;
+          $scope.owner.numReviews = $scope.owner.rating.length;
+      }, function(err) {
+          $location.path('/spot');
+      });
+
+
+
+
       $scope.host = e.data;
       if($scope.host.rating.quality.length <= 0) {
           $scope.host.rating.quality = [3];
