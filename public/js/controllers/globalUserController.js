605932545e1810c4f4c0d482425dd4796c5d42b6
Jean-Baptiste <contact@jbpasquier.eu>
2016-06-09 15:58:53 +0200

    Clean & replace places

--- public/js/controllers/globalUserController.js
+++ public/js/controllers/globalUserController.js
@@ -1,7 +1,7 @@
 function globalUserController($scope, $window, $rootScope) {
 
     $scope.currentUser = JSON.parse($window.localStorage.getItem('currentUser'));
-    $rootScope.$on('userUpdated', function(event, user){
+    $rootScope.$on('userUpdated', function(event, user) {
         $scope.currentUser = JSON.parse($window.localStorage.getItem('currentUser'));
     });
 
--- public/js/services/spotsService.js
+++ public/js/services/spotsService.js
@@ -1,20 +1,20 @@
 angular.module('app')
     .service('spotsService', function($http) {
-    return {
-        get: function() {
-            return $http.get('/spots');
-        },
-        getOne: function(id) {
-            return $http.get('/spots/' + id);
-        },
-        update: function(id, data) {
-            return $http.put('/spots/' + id, data);
-        },
-        create: function(data) {
-            return $http.post('/spots', data);
-        },
-        delete: function(id) {
-            return $http.delete('/spots/' + id);
-        }
-    };
-});
+        return {
+            get: function() {
+                return $http.get('/spots');
+            },
+            getOne: function(id) {
+                return $http.get('/spots/' + id);
+            },
+            update: function(id, data) {
+                return $http.put('/spots/' + id, data);
+            },
+            create: function(data) {
+                return $http.post('/spots', data);
+            },
+            delete: function(id) {
+                return $http.delete('/spots/' + id);
+            }
+        };
+    });
