5988ad346d8c16184c2fe4e453456873200c9e0a
Paul Crayssac <paul-ka@laposte.net>
2016-06-14 10:44:18 +0200

    Impove spots cration

--- public/js/controllers/createSpotsController.js
+++ public/js/controllers/createSpotsController.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('createSpotsController', function($scope, $http, $q, $window, $location, spotsFactory, spotsService) {
+angular.module('app').controller('createSpotsController', function($scope, $http, $q, $window, $location, $routeParams, spotsFactory, spotsService) {
 
     if ($window.localStorage.currentUser) $scope.currentUser = JSON.parse($window.localStorage.getItem('currentUser'));
     else $scope.currentUser = {
@@ -23,20 +23,61 @@ angular.module('app').controller('createSpotsController', function($scope, $http
     };
     resetObj();
     $scope.step = 1;
+    if ($routeParams.id) {
+        $scope.isAction = 'modification';
+        spotsService.getOne($routeParams.id).then(function(res) {
+            if (res.data.owner != $scope.currentUser._id) $location.path('/');
+            $scope.obj = res.data;
+            $scope.obj.modification = new Date();
+        })
+    } else {
+        $scope.isAction = 'création';
+    }
+    function gmapGeocode() {
+        var defer = $q.defer(),
+            addr = ($scope.obj.address.num || '') + ' ' + ($scope.obj.address.road || '') + ' ' + ($scope.obj.address.postalCode || '') + ' ' + $scope.obj.address.city + ' ' + $scope.obj.address.country;
+            console.log(addr);
+        if (!this.geocoder) this.geocoder = new google.maps.Geocoder();
+        this.geocoder.geocode({
+            'address': addr
+        }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                var loc = results[0].geometry.location;
+                $scope.obj.latitude = loc.lat();
+                $scope.obj.longitude = loc.lng();
+                defer.resolve();
+            } else {
+                defer.reject('Adresse introuvable');
+            }
+        });
+        return defer.promise;
+    }
     $scope.send = function() {
-        console.dir($scope.obj);
-        spotsService.create({
-            content: $scope.obj
-        }).then(function(res) {
-            console.log('Spot créée');
-            $scope.obj = {};
-            resetObj();
-            $location.path('/picture/spots/0/'+res.data._id);
+        gmapGeocode().then(function() {
+            var act;
+            if ($scope.isAction == 'création') {
+                act = spotsService.create({
+                    content: $scope.obj
+                })
+            } else {
+                act = spotsService.update($scope.obj._id, {
+                    content: $scope.obj
+                })
+            }
+            act.then(function(res) {
+                $scope.obj = {};
+                resetObj();
+                $location.path('/picture/spots/0/' + res.data._id);
+            }, function(err) {
+                $scope.error = err;
+            });
+        }, function(err) {
+            $scope.error = err;
         });
     };
 
-    $scope.places = {};
-    spotsService.get().then(function(res) {
+/*    spotsService.get().then(function(res) {
         $scope.places = res.data;
     });
+*/
 });
