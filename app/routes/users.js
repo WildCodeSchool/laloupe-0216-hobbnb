a27e5b0ac5f8d0984cbe3dd70f6477ab1c66c349
Jean-Baptiste <contact@jbpasquier.eu>
2016-06-14 11:40:52 +0200

    Bugfix // Searhc reset place

--- app/routes/users.js
+++ app/routes/users.js
@@ -13,6 +13,8 @@ module.exports = function(app) {
 
     usersRouter.post('/login', Users.login);
 
+    usersRouter.get('/:id/:token', Users.confirm);
+    
     usersRouter.get('/:id', Auth.user.hasAuthorization, Users.findOne);
 
     usersRouter.post('/', Users.create);
