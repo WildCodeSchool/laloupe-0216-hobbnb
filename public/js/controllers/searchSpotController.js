3f80bcb7d64d21d7cb94ed20bf4243e5237abb9a
Paul Crayssac <paul-ka@laposte.net>
2016-06-20 11:46:39 +0200

    Spot spot spot ! :)

--- public/js/controllers/searchSpotController.js
+++ public/js/controllers/searchSpotController.js
@@ -19,8 +19,6 @@ angular.module('app').controller('searchSpotController', function($scope, $http,
     $scope.definitiveFilter = {};
     spotsService.get().then(function(res) {
         $scope.positions = res.data;
-
-
         $scope.positions.map(function(e) {
             if (e.rating.quality.length <= 0) {
                 e.rating.quality = [3];
@@ -36,61 +34,61 @@ angular.module('app').controller('searchSpotController', function($scope, $http,
             });
             return e;
         });
-    });
-    $scope.$watchCollection('filters', function(newCol, oldCol) {
-        if (!!newCol.hobby) {
-            if (!$scope.definitiveFilter.spot) $scope.definitiveFilter.spot = [];
-            if (newCol.hobby == 'Peu importe') {
-                $scope.filters.hobby = '';
-                if (!!$scope.definitiveFilter.spot.primarySports) delete $scope.definitiveFilter.spot.primarySports;
-            } else {
-                $scope.definitiveFilter.spot.primarySports = newCol.hobby;
+        $scope.$watchCollection('filters', function(newCol, oldCol) {
+            if (!!newCol.hobby) {
+                if (!$scope.definitiveFilter.spot) $scope.definitiveFilter.spot = [];
+                if (newCol.hobby == 'Peu importe') {
+                    $scope.filters.hobby = '';
+                    if (!!$scope.definitiveFilter.spot.primarySports) delete $scope.definitiveFilter.spot.primarySports;
+                } else {
+                    $scope.definitiveFilter.spot.primarySports = newCol.hobby;
+                }
             }
-        }
-        // model sur http://jsfiddle.net/Wijmo/Rqcsj/
-        if (!!newCol.place) {
-            clearTimeout(delay);
-            delay = setTimeout(function() {
-                if (!this.geocoder) this.geocoder = new google.maps.Geocoder();
-                this.geocoder.geocode({
-                    'address': newCol.place
-                }, function(results, status) {
-//                    console.dir($scope.definitiveFilter);
-                    if (status == google.maps.GeocoderStatus.OK) {
-                        var loc = results[0].geometry.location;
-                        $scope.latitude = loc.lat();
-                        $scope.longitude = loc.lng();
-                        $scope.latitudemin = $scope.latitude - 0.5;
-                        $scope.latitudemax = $scope.latitude + 0.5;
-                        $scope.kmbydegree = (111 * Math.cos($scope.latitude));
-                        $scope.longitudemin = $scope.longitude + 35 / $scope.kmbydegree;
-                        $scope.longitudemax = $scope.longitude - 35 / $scope.kmbydegree;
-                    } else {
-//                        console.dir('trouve pas la place');
-                        delete $scope.latitude;
-                        delete $scope.longitude;
-                        delete $scope.latitudemax;
-                        delete $scope.latitudemin;
-                        delete $scope.kmbydegree;
-                        delete $scope.longitudemax;
-                        delete $scope.longitudemin;
-                    }
-                });
-            }, 1000);
-        } else {
-//            console.dir($scope.definitiveFilter);
-            delete $scope.latitude;
-            delete $scope.longitude;
-            delete $scope.latitudemax;
-            delete $scope.latitudemin;
-            delete $scope.kmbydegree;
-            delete $scope.longitudemax;
-            delete $scope.longitudemin;
-        }
-        //Ask from over page
-        $scope.filters.place = searchFactory.data.city;
-        $scope.filters.hobby = searchFactory.data.hobby;
+            // model sur http://jsfiddle.net/Wijmo/Rqcsj/
+            if (!!newCol.place) {
+                clearTimeout(delay);
+                delay = setTimeout(function() {
+                    if (!this.geocoder) this.geocoder = new google.maps.Geocoder();
+                    this.geocoder.geocode({
+                        'address': newCol.place
+                    }, function(results, status) {
+                        //                    console.dir($scope.definitiveFilter);
+                        if (status == google.maps.GeocoderStatus.OK) {
+                            var loc = results[0].geometry.location;
+                            $scope.latitude = loc.lat();
+                            $scope.longitude = loc.lng();
+                            $scope.latitudemin = $scope.latitude - 0.5;
+                            $scope.latitudemax = $scope.latitude + 0.5;
+                            $scope.kmbydegree = (111 * Math.cos($scope.latitude));
+                            $scope.longitudemin = $scope.longitude + 35 / $scope.kmbydegree;
+                            $scope.longitudemax = $scope.longitude - 35 / $scope.kmbydegree;
+                        } else {
+                            //                        console.dir('trouve pas la place');
+                            delete $scope.latitude;
+                            delete $scope.longitude;
+                            delete $scope.latitudemax;
+                            delete $scope.latitudemin;
+                            delete $scope.kmbydegree;
+                            delete $scope.longitudemax;
+                            delete $scope.longitudemin;
+                        }
+                    });
+                }, 1000);
+            } else {
+                //            console.dir($scope.definitiveFilter);
+                delete $scope.latitude;
+                delete $scope.longitude;
+                delete $scope.latitudemax;
+                delete $scope.latitudemin;
+                delete $scope.kmbydegree;
+                delete $scope.longitudemax;
+                delete $scope.longitudemin;
+            }
+            //Ask from over page
+            $scope.filters.place = searchFactory.data.city;
+            $scope.filters.hobby = searchFactory.data.hobby;
 
+        });
     });
 
 
@@ -140,10 +138,10 @@ angular.module('app').controller('searchSpotController', function($scope, $http,
         var url = "uploads/spots/" + widget._id + "/" + widget.picture;
         return "{'background-image': 'url(" + url + ")', 'background-size': 'cover'}";
     };
-    $scope.locationToData = function(){
+    $scope.locationToData = function() {
         searchFactory.data.city = $scope.filters.place;
     }
-    $scope.hobbyToData = function(){
+    $scope.hobbyToData = function() {
         console.log($scope.filters.hobby);
         searchFactory.data.hobby = $scope.filters.hobby;
     }
