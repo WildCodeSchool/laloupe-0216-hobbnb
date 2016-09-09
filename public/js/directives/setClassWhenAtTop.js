19108059e0fdbcde5dc384b9a91e57e8d57f3b91
IbanGF <ueqiban@gmail.com>
2016-09-09 19:03:13 +0200

    refont of show spot

--- public/js/directives/setClassWhenAtTop.js
+++ public/js/directives/setClassWhenAtTop.js
@@ -5,21 +5,14 @@ angular.module('app')
             restrict: 'A',
             link: function(scope, element, attrs) {
                 var topClass = attrs.setClassWhenAtTop,
-                    offsetTop = element.offset().top,
-                    bottomElementOffset = angular.element(document.querySelector('#location'));
-                console.log(offsetTop);
-                console.log(bottomElementOffset);
+                    offsetTop = element.offset().top;
                 $win.on('scroll', function(e) {
                     if ($win.scrollTop() >= offsetTop) {
                         element.addClass(topClass);
                         scope.topReached = true;
                     } else {
-                      console.log(document.body.scrollTop);
-                      console.log(element[0].offsetHeight + offsetTop);
-                      console.log(element[0].scrollHeight);
                         element.removeClass(topClass);
                         scope.topReached = false;
-                        scope.$apply();
                     }
                     scope.$apply();
                 });
